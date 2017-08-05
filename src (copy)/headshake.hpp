84ab2f5819b70792348ee8dca75ac842a22205e7
nullifiedcat <nullifiedcat@gmail.com>
2017-08-05 09:13:17 +0300

    Revert "delete copies lol"
    This reverts commit f5b2cbc9292fa38e6ca576264229743012657b50.

---
+++ src (copy)/headshake.hpp
@@ -0,0 +1,19 @@
+/*
+ * headshake.hpp
+ *
+ *  Created on: Aug 2, 2017
+ *      Author: nullifiedcat
+ */
+
+#pragma once
+
+class CatVar;
+
+namespace headshake {
+
+extern CatVar enable_hs;
+
+void Update();
+void CreateMove();
+
+}
